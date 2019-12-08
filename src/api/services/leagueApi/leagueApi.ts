c5694208b964b69bf2f90ea16e980b2068f87d3e
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-12-08 14:36:34 -0500

    Refactored leagueApi to call common axios function

--- src/api/services/leagueApi/leagueApi.ts
+++ src/api/services/leagueApi/leagueApi.ts
@@ -3,57 +3,58 @@ import axios from 'axios'
 import * as config from '../../../../config'
 import { Summoner, Match, MatchMetadata, MatchTimeline } from '../../../shared-types'
 
-const getSummonerDetails = async (summonerName): Promise<Summoner | null> => {
-    const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}`
+const makeLeagueRequest = async (url, apiNameForErrorHandling, params = {}) => {
     try {
         const response = await axios({
             method: 'GET',
             url: url,
             headers: {
                 "X-Riot-Token": config.LEAGUE_API_KEY,
-            }
+            },
+            params
         })
-        console.log("getSummonerDetails", response.status)
+
         if (response.status === 200) {
             return response.data
-        } else if (response.status === 404) {
+        } else if (
+            response.status === 404
+            || response.status === 403
+            || response.status === 429
+        ) {
+            console.log(`Received status code: ${response.status} for ${apiNameForErrorHandling}`)
             return null
         } else {
-            throw new Error(`Failed to call getSummonerDetails: ${response.status}`)
+            throw new Error(`Failed to call ${apiNameForErrorHandling}: ${response.status}`)
         }
     } catch (error) {
-        console.log(`Failed to call getSummonerDetails: ${JSON.stringify(error.message)}`)
+        console.log(`Failed to call ${apiNameForErrorHandling}: ${JSON.stringify(error.message)}`)
         return null
     }
 }
 
-const getSummonerMatches = async (accountId: string, beginIndex: number = 0, endIndex: number): Promise<Match[]> => {
-    const url = `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}`
-    const response = await axios({
-        method: 'GET',
-        url: url,
-        headers: {
-            "X-Riot-Token": config.LEAGUE_API_KEY,
-        },
-        params: {
-            beginIndex,
-            endIndex
-        }
-    })
+const getSummonerDetails = async (summonerName): Promise<Summoner | null> => {
+    const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodeURIComponent(summonerName)}`
+    const summonerDetails = await makeLeagueRequest(url, 'getSummonerDetails')
+    return summonerDetails
+}
 
-    if (response.status === 200) {
-        const matchesMod = response.data.matches.map(({ timestamp, ...rest }) => ({
+const getSummonerMatches = async (accountId: string, beginIndex: number = 0, endIndex: number): Promise<Match[] | null> => {
+    const url = `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}`
+    const params = {
+        beginIndex,
+        endIndex
+    }
+    const data = await makeLeagueRequest(url, 'getSummonerMatches', params)
+
+    if (data.matches) {
+        const matchesMod = data.matches.map(({ timestamp, ...rest }) => ({
             timestamp: new Date(timestamp),
             ...rest,
             accountId
         }))
         return matchesMod
-    }
-    if (response.status === 404 || response.status === 429) {
-        console.log('Rate Limit exceeded or 404 with getSummonerMatches')
-        return []
     } else {
-        throw new Error('Failed to call getSummonerMatches')
+        return null
     }
 }
 
@@ -63,10 +64,10 @@ const bootstrapSummonerMatches = async (accountId: string): Promise<Match[] | nu
     const matches: Match[] = []
 
     while (beginIndex < 20) { // This can in increased later. For now get it working with 100.
-        const response = await getSummonerMatches(accountId, beginIndex, endIndex)
+        const matchSubset = await getSummonerMatches(accountId, beginIndex, endIndex)
 
-        if (response && response.length) {
-            matches.push(...response)
+        if (matchSubset && matchSubset.length) {
+            matches.push(...matchSubset)
             beginIndex += 100
         } else {
             break
@@ -77,48 +78,14 @@ const bootstrapSummonerMatches = async (accountId: string): Promise<Match[] | nu
 
 const getMatchMetadata = async (gameId: number): Promise<MatchMetadata | null> => {
     const url = `https://na1.api.riotgames.com/lol/match/v4/matches/${gameId}`
-    const response = await axios({
-        method: 'GET',
-        url: url,
-        headers: {
-            "X-Riot-Token": config.LEAGUE_API_KEY,
-        },
-        params: {
-        }
-    })
-
-    if (response.status === 200) {
-        return response.data
-    }
-    if (response.status === 404 || response.status === 429) {
-        console.log('Rate Limit exceeded or 404 with getMatchMetadata')
-        return null
-    } else {
-        throw new Error('Failed to call getMatchMetadata')
-    }
+    const matchData = await makeLeagueRequest(url, 'getMatchData')
+    return matchData
 }
 
 const getMatchTimeline = async (gameId: number): Promise<MatchTimeline | null> => {
     const url = `https://na1.api.riotgames.com/lol/match/v4/timelines/by-match/${gameId}`
-    const response = await axios({
-        method: 'GET',
-        url: url,
-        headers: {
-            "X-Riot-Token": config.LEAGUE_API_KEY,
-        },
-        params: {
-        }
-    })
-
-    if (response.status === 200) {
-        return response.data
-    }
-    if (response.status === 404 || response.status === 429) {
-        console.log('Rate Limit exceeded or 404 with getMatchTimeline')
-        return null
-    } else {
-        throw new Error('Failed to call getMatchTimeline')
-    }
+    const matchTimeline = await makeLeagueRequest(url, 'getMatchTimeline')
+    return matchTimeline
 }
 
 export { getSummonerDetails, getSummonerMatches, bootstrapSummonerMatches, getMatchMetadata, getMatchTimeline }
