13b148befea12d93174a051deec74e84fdded68a
Travis Bumgarner <travis.bumgarner@gmail.com>
2019-12-08 17:23:00 -0500

    cleanup dead code

--- src/api/database/matchTimeline.ts
+++ src/api/database/matchTimeline.ts
@@ -1,38 +1,6 @@
 import knex from '.'
-import { uuid } from 'uuidv4';
 
 import { MatchTimeline } from '../../shared-types'
-
-// const insertParticipantFrames = async (participantFramesId: string, participantFrames: ParticipantFrames) => {
-//     const participantFramesMod = Object.values(participantFrames).map(participantFrame => {
-//         const {
-//             totalGold,
-//             teamScore,
-//             participantId,
-//             level,
-//             currentGold,
-//             minionsKilled,
-//             dominionScore,
-//             xp,
-//             jungleMinionsKilled
-//         } = participantFrame
-
-//         return {
-//             totalGold,
-//             teamScore,
-//             participantId,
-//             level,
-//             currentGold,
-//             minionsKilled,
-//             dominionScore,
-//             xp,
-//             jungleMinionsKilled,
-//             id: participantFramesId
-//         }
-//     })
-//     const response = await knex('participantFrames').insert(participantFramesMod)
-//     return
-// }
 
 const insert = async (gameId: number, matchTimeline: MatchTimeline) => {
     return knex('matchTimeline').insert(matchTimeline)
